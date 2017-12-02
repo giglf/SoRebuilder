a79b128c4d6a62178fd2e784458a00e736f98006
giglf <lin.giglf@gmail.com>
2017-12-02 15:43:17 +0800

    Make log have wonderful color support.
    Red for error
    Blue for debug
    Yellow for verbose
    White for normal
    In order to distinguish between different log.

--- src/Log.h
+++ src/Log.h
@@ -7,9 +7,17 @@ extern bool VERBOSE;
 extern bool DEBUG;
 
 
-#define VLOG(fmt, ...) if(VERBOSE) printf(fmt "\n", ##__VA_ARGS__)			//verbose log
-#define DLOG(fmt, ...) if(DEBUG) printf("[DEBUG] " fmt "\n", ##__VA_ARGS__)	//debug log
+#define NONE 	"\e[0m"   				// end of the ansi control
+#define BLUE 	"\e[0;34m"				// Blue color, normal
+#define B_BLUE 	"\e[1;34m"				// Blue color, bold
+#define RED 	"\e[0;31m"				// Red color, normal
+#define B_RED	"\e[1;31m"				// Red color, bold
+#define YELLOW	"\e[0;33m"				// Yellow color
+
+
+#define VLOG(fmt, ...) if(VERBOSE) printf(YELLOW fmt "\n" NONE, ##__VA_ARGS__)			//verbose log
+#define DLOG(fmt, ...) if(DEBUG) printf(B_BLUE "[DEBUG] " BLUE fmt "\n" NONE, ##__VA_ARGS__)	//debug log
 #define LOG(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)						//normal log
-#define ELOG(fmt, ...) printf("[ERROR] " fmt "\n", ##__VA_ARGS__)				//error log
+#define ELOG(fmt, ...) printf(B_RED "[ERROR] " RED fmt "\n" NONE, ##__VA_ARGS__)				//error log
 
 #endif
